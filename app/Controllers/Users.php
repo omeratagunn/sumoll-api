10a518e9adf247347936d8af1532ed72857aaf98
unknown <omratagn@gmail.com>
2021-05-17 22:33:35 +0200

    controller to model rather than repository

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -6,7 +6,7 @@ use sumollapi\Config\Config;
 use sumollapi\Database\Database;
 use sumollapi\Database\NoSql\Redis;
 use sumollapi\Helpers\StatusCodes;
-use sumollapi\Repository\UserRepository;
+use sumollapi\Models\User;
 use sumollapi\Response\Response;
 
 
@@ -15,7 +15,7 @@ class Users implements ControllerInterface
 
     public function getUserDetails(int $id = null){
         $config = new Config();
-        $repo = new UserRepository(new Database($config), new Redis($config));
+        $repo = new User(new Database($config), new Redis($config));
         $userDetails = $repo->read($id);
             if ($userDetails)
             return new Response($userDetails, StatusCodes::$success);
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 
 namespace sumollapi\Models;
 
+use sumollapi\Database\DatabaseInterface;
+use sumollapi\Database\NoSql\NoSqlInterface;
 use sumollapi\Repository\UserRepository;
 
 class User extends UserRepository
@@ -10,9 +12,13 @@ class User extends UserRepository
 
     protected int $id;
     protected array $userDetails;
+    public function __construct(DatabaseInterface $database, NoSqlInterface $redis)
+    {
+        parent::__construct($database, $redis);
+    }
 
     public function get(int $id){
-        return $this->userDetails = (new UserRepository())->read($id);
+        return $this->userDetails = $this->read($id);
     }
 
     public function update(int $id, array $data){
